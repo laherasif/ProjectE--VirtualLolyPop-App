dd2051b8567c0eabae072611e81d73eff02186b5
laherasif <laherasif@gmail.com>
2020-11-29 06:34:11 -0800

    fun chnage

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,31 +1,31 @@
-const path = require(`path`)
+// const path = require(`path`)
 
-exports.createPages = async ({actions, graphql}) => {
-    const {createPage} = actions;
+// exports.createPages = async ({actions, graphql}) => {
+//     const {createPage} = actions;
 
 
-  const result = await graphql(`
-  query MyQuery {
-    Lollies {
-      LolyData {
-        link
-      }
-    }
-  }
+//   const result = await graphql(`
+//   query MyQuery {
+//     Lollies {
+//       LolyData {
+//         link
+//       }
+//     }
+//   }
   
 
-  `) 
-  console.log("result is " ,JSON.stringify(result.data))
+//   `) 
+//   console.log("result is " ,JSON.stringify(result.data))
 
 
-  result.data.Lollies.LolyData.forEach(( obj) => {
-      console.log("obj" , obj)
-    createPage({
-      path: `/lolies/${obj.link}`,
-      component: path.resolve(`./src/component/template.tsx`),
-      context: {
-        lolyDetails: obj
-    }
-    })
-  })
-}
+//   result.data.Lollies.LolyData.forEach(( obj) => {
+//       console.log("obj" , obj)
+//     createPage({
+//       path: `/lolies/${obj.link}`,
+//       component: path.resolve(`./src/component/template.tsx`),
+//       context: {
+//         lolyDetails: obj
+//     }
+//     })
+//   })
+// }
