228d20e4780074e7afeb8642e135eb2273b98424
laherasif <laherasif@gmail.com>
2020-11-29 06:28:12 -0800

    chnages are

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
     plugins: [
-        {
-            resolve: "gatsby-source-graphql",
-            options: {
-              typeName: "Lolly",
-              fieldName: "Lollies",
-              url: "",
-              // http://localhost:8888/.netlify/functions/lolly
-            },
-          },
+        // {
+        //     resolve: "gatsby-source-graphql",
+        //     options: {
+        //       typeName: "Lolly",
+        //       fieldName: "Lollies",
+        //       url: "",
+        //       http://localhost:8888/.netlify/functions/lolly
+        //     },
+        //   },
           
     ]
 }
